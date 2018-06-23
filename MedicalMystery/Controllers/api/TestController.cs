07a10207521b4cdc7dcab99b1a2d5bc53664218b
Christo Aruste <caruste@itcollege.ee>
2018-06-23 22:42:53 +0300

    Front end almost complete- must fix the question part.

--- MedicalMystery/Controllers/api/TestController.cs
+++ MedicalMystery/Controllers/api/TestController.cs
@@ -65,14 +65,13 @@ namespace MedicalMystery.Controllers
                                     .Select(x => x.Key.ToString())
                                     .Take(3)
                                     .ToList();
-
             return sortedDictionary;
         }
 
         [HttpPost]
         public IActionResult CheckSymptoms([FromBody] List<string> list)
         {
-            if (list.Count == 0)return NotFound();
+            if (list.Count == 0) return NotFound();
             List<Disease> diseases = _dataContext.All();
             List<Disease> final = new List<Disease>();
 
@@ -92,6 +91,61 @@ namespace MedicalMystery.Controllers
             return Ok();
         }
 
+        [HttpPost("ForQuestions")]
+        public IActionResult GetNextQuestion([FromBody] Input input)
+        {
+            List<Disease> diseases = _dataContext.All();
+            List<Disease> hasAll = new List<Disease>();
+
+            if (input.positive != null)
+            {
+                if (input.positive.Count == 0)
+                {
+                    hasAll = diseases;
+                }
+                else
+                {
+                    foreach (Disease item in diseases)
+                    {
+                        if (ContainsAllItems(item.symptoms, input.positive)) hasAll.Add(item);
+                    }
+                }
+            }
+
+            List<Disease> final = new List<Disease>();
+
+            if (input.negative != null)
+            {
+                if (input.negative.Count == 0)
+                {
+                    final = hasAll;
+                }
+                else
+                {
+                    foreach (Disease item in hasAll)
+                    {
+                        final = hasAll.Where(x => !input.negative.Any(t => input.negative.Contains(t))).ToList(); 
+                    }   
+                }
+            }
+
+            var list = final.OrderByDescending(x => x.symptoms.Count);
+            Disease disease = new Disease();
+            if (list.Count() <= 1) return Ok(list.FirstOrDefault());
+            if (list.FirstOrDefault().symptoms.Count == input.positive.Count) disease = list.ElementAt(1);
+
+#warning Must find diferences between the lists and use the element from that. If one has symptom#1 and another one has the same one then dont ask that. Must ask the one which both dont have.
+
+            else disease = list.FirstOrDefault();
+
+            foreach (var item in disease.symptoms)
+            {
+                if (!input.positive.Contains(item) && !input.negative.Contains(item)) return Ok(item);
+            }
+
+            return NotFound();
+        }
+
         public static bool ContainsAllItems(List<string> a, List<string> b)
         {
             return !b.ConvertAll(x => x.ToLower()).Except(a.ConvertAll(x => x.ToLower())).Any();
@@ -99,5 +153,10 @@ namespace MedicalMystery.Controllers
 #warning Setup database now so it would be easier later!
 #warning Must add a way to add String[] types to the database
 #warning Must start a database
+    }
+    public class Input
+    {
+        public List<string> positive;
+        public List<string> negative;
     }
 }
